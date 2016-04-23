aa47d66067afb5e42fc5aa1fce6ace81165471de
limeng <lm2343635@126.com>
2016-04-23 16:14:14 +0900

    Initial Commit

---
+++ GroupFinance/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GroupFinance
+//
+//  Created by lidaye on 4/23/16.
+//  Copyright © 2016 limeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
