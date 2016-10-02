1a6d3ef972106798bf451fa805a0553f445e03d9
limeng <lm2343635@126.com>
2016-10-02 18:42:03 +0900

    Add UserTool; Remove pod files

---
+++ GroupFinance/SettingsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsTableViewController.h
+//  GroupFinance
+//
+//  Created by lidaye on 25/09/2016.
+//  Copyright © 2016 limeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsTableViewController : UITableViewController
+
+@end
---
+++ GroupFinance/SettingsTableViewController.m
@@ -0,0 +1,30 @@
+//
+//  SettingsTableViewController.m
+//  GroupFinance
+//
+//  Created by lidaye on 25/09/2016.
+//  Copyright © 2016 limeng. All rights reserved.
+//
+
+#import "SettingsTableViewController.h"
+
+@interface SettingsTableViewController ()
+
+@end
+
+@implementation SettingsTableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+}
+
+#pragma mark - Table view data source
+- (CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section {
+    if(DEBUG) {
+        NSLog(@"Running %@ '%@'", self.class, NSStringFromSelector(_cmd));
+    }
+    return 0.1;
+}
+
+@end
