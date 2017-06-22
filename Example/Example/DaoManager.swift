55ed3c84e6989a2799947e34382677319243b308
limeng <lm2343635@126.com>
2017-06-22 19:44:36 +0900

    Add Test entity.

---
+++ Example/Example/DaoManager.swift
@@ -0,0 +1,42 @@
+//
+//  DaoManager.swift
+//  Httper
+//
+//  Created by 李大爷的电脑 on 22/01/2017.
+//  Copyright © 2017 limeng. All rights reserved.
+//
+
+import CoreData
+import UIKit
+
+class DaoManager {
+    
+    let context: NSManagedObjectContext!
+    let testDao: TestDao!
+    
+    static let sharedInstance : DaoManager = {
+        let instance = DaoManager()
+        return instance
+    }()
+    
+    init() {
+        let delegate = UIApplication.shared.delegate as! AppDelegate
+        context = delegate.dataStack.mainContext
+        testDao = TestDao(context)
+    }
+    
+    func saveContext() {
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nserror = error as NSError
+                print("Unresolved error \(nserror), \(nserror.userInfo)")
+                abort()
+            }
+        }
+    }
+
+}
---
+++ Example/Example/TestDao.swift
@@ -0,0 +1,13 @@
+//
+//  testDao.swift
+//  Example
+//
+//  Created by lidaye on 22/06/2017.
+//  Copyright © 2017 Softlab. All rights reserved.
+//
+
+import UIKit
+
+class TestDao: DaoTemplate {
+
+}
